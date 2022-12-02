f26a611359cb3fa782c5820ff564b0a83ace591c
Kyle Macquarrie <kyle_macquarrie@next.co.uk>
2022-12-02 09:09:59 +0000

    Add prettier config and format

--- day1/1.mjs
+++ day1/1.mjs
@@ -1,18 +1,18 @@
-import { readFile } from "node:fs/promises";
+import { readFile } from 'node:fs/promises'
 
-const data = await readFile("./input.txt", { encoding: "utf8" });
-const chunks = data.toString().split("\n\n");
+const data = await readFile('./input.txt', { encoding: 'utf8' })
+const chunks = data.toString().split('\n\n')
 
 const totals = chunks
   .map((chunk) => {
-    let total = 0;
-    chunk.split("\n").forEach((i) => (total += parseInt(i)));
-    return total;
+    let total = 0
+    chunk.split('\n').forEach((i) => (total += parseInt(i)))
+    return total
   })
-  .sort((a, b) => b - a);
+  .sort((a, b) => b - a)
 
-const answerPart1 = totals[0];
-console.log({ answerPart1 });
+const answerPart1 = totals[0]
+console.log({ answerPart1 })
 
-const answerPart2 = totals[0] + totals[1] + totals[2];
-console.log({ answerPart2 });
+const answerPart2 = totals[0] + totals[1] + totals[2]
+console.log({ answerPart2 })
