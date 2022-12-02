df1bc42d0251e50c848f513c6c1f899a2e75fbaf
Kyle Macquarrie <kyle_macquarrie@next.co.uk>
2022-12-02 10:16:17 +0000

    Refactor day 1 to more functional style

--- day1/1.mjs
+++ day1/1.mjs
@@ -4,15 +4,12 @@ const data = await readFile('./input.txt', { encoding: 'utf8' })
 const chunks = data.toString().split('\n\n')
 
 const totals = chunks
-  .map((chunk) => {
-    let total = 0
-    chunk.split('\n').forEach((i) => (total += parseInt(i)))
-    return total
-  })
+  .map((chunk) => chunk.split('\n'))
+  .map((arr) => arr.reduce((acc, curr) => acc + parseInt(curr), 0))
   .sort((a, b) => b - a)
 
 const answerPart1 = totals[0]
-console.log({ answerPart1 })
+console.log({ answerPart1, isCorrect: answerPart1 === 67450 })
 
-const answerPart2 = totals[0] + totals[1] + totals[2]
-console.log({ answerPart2 })
+const answerPart2 = totals.slice(0, 3).reduce((acc, curr) => acc + curr, 0)
+console.log({ answerPart2, isCorrect: answerPart2 === 199357 })
