0d5918caa2934666845905731614189f69821de0
Kyle Macquarrie <kyle_macquarrie@next.co.uk>
2022-12-06 11:23:12 +0000

    Refactor day 6

--- day6/index.ts
+++ day6/index.ts
@@ -2,25 +2,15 @@ const input = await Deno.readTextFile('./input.txt').then((str) =>
   str.split('\n').filter(Boolean)[0].split('')
 )
 
-let answerPart1 = 0
-let answerPart2 = 0
-
-for (let i = 3; i < input.length; i++) {
-  const test = [...new Set(input.slice(i - 3, i + 1))].length === 4
-  if (test) {
-    answerPart1 = i + 1
-    break
+function test(count: number) {
+  for (let i = count - 1; i < input.length; i++) {
+    const test = [...new Set(input.slice(i - (count - 1), i + 1))]
+    if (test.length === count) return i + 1
   }
 }
+
+const answerPart1 = test(4)
+const answerPart2 = test(14)
 
 console.log({ answerPart1, isCorrect: answerPart1 === 1343 })
-
-for (let i = 13; i < input.length; i++) {
-  const test = [...new Set(input.slice(i - 13, i + 1))].length === 14
-  if (test) {
-    answerPart2 = i + 1
-    break
-  }
-}
-
 console.log({ answerPart2, isCorrect: answerPart2 === 2193 })
